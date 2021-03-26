e351ae0ae8a3497d2ef35f947debc434d78f2a25
saedhamad-dev <saedhamad120@gmail.com>
2021-03-26 18:06:21 +0200

    new update

--- src/BMI.py
+++ src/BMI.py
@@ -7,15 +7,15 @@ import tkinter as tk
 from tkinter import messagebox
 import webbrowser
 import tkinter.ttk as ttk
-
+# create connection to BMI database
 cn = mysql.connector.connect(host='localhost',
                              user='root',
                              password='',
                              database="BMI")
 cr = cn.cursor()
-cr1 = cn.cursor()
+# if there is no database here we created it
 cr.execute("create database if not exists BMI")
-
+# creating table in the bmi database (report)
 cr.execute('''create table if not exists Report (
                         Id_Inc int auto_increment, 
                         Name varchar(25) not null,
@@ -26,15 +26,15 @@ cr.execute('''create table if not exists Report (
 
 
 Indextabl = 0
-sqs = "SELECT * FROM BMI.Report order by id_inc"
-cr.execute(sqs)
-tbl = cr.fetchall()
+sqs = "SELECT * FROM BMI.Report order by id_inc"#query to select (all(*)) from db table ordered by id
+cr.execute(sqs)#executes the sqs to run the query
+tbl = cr.fetchall()#fetchall is a query result set and returns a list of tuples
 
-
+#def exit for window protocol and for exit menu
 def Exit():
-    cr.close()
-    cn.close()
-    win.destroy()
+    cr.close()# close the cursor
+    cn.close()# close the connection
+    win.destroy()# destroys the window
 
 
 def is_num(x):
@@ -45,15 +45,15 @@ def is_num(x):
     else:
         return True
 
-
+#clearing al fields and results
 def Clear(a=""):
-    v1.set('')
-    v2.set('')
-    v3.set('')
-    Clear_Lb()
-    e1.focus()
+    v1.set('')#sets the stingvar of name entry ''(nothing)
+    v2.set('')#sets the stingvar of weight entry ''(nothing)
+    v3.set('')#sets the stingvar of height entry ''(nothing)
+    Clear_Lb()#change the color for the category result
+    e1.focus()#sets the name entry to enter data
 
-
+#resetting result labels to be unshown
 def Clear_Lb(a=""):
     lb11.config(text='     ')
     lb12.config(fg='lightgray')
@@ -63,76 +63,76 @@ def Clear_Lb(a=""):
     lb17.config(fg='lightgray')
     lb18.config(fg='lightgray')
 
-
+#saving every thing to database and checks if any field is empty or not
 def Save(a=""):
-    if (not is_num(e2.get())) or float(e2.get()) <= 0:
-        e2.focus()
-        v2.set('')
+    if (not is_num(e2.get())) or float(e2.get()) <= 0:#checks if weight entry isn't a number or float
+        e2.focus()#puts the mouse in the weight entry
+        v2.set('')# sets nothing in the weight entry
         return
 
-    if (not is_num(e3.get())) or float(e2.get()) <= 0:
-        e3.focus()
-        v3.set('')
+    if (not is_num(e3.get())) or float(e2.get()) <= 0:#checks if height entry isn't a number or float
+        e3.focus()#puts the mouse in the height entry
+        v3.set('')#sets nothing in the height entry
         return
-    d2 = datetime.strptime(e4.get(), '%d/%m/%Y')
-    if todaydat <= d2:
-        showerror('Update', 'Date should be < System date')
+    d2 = datetime.strptime(e4.get(), '%d/%m/%Y')#strptime() method creates a datetime object from the given string
+    if todaydat <= d2:# if today date is less than or equal to date time of now
+        showerror('Update', 'Date should be < System date')# shows messagebox 
         return False
-    Calcul()
-    quest = messagebox.askyesno("Save", "Do you want to save data ?")
-    if quest == True:
+    Calcul()# calculates the gives height and weight
+    quest = messagebox.askyesno("Save", "Do you want to save data ?")# asks yes or no if you really want to save
+    if quest == True:# if quest = yes then ...
         sq = '''insert into BMI.Report (Name, Weight, Height, B_Date) 
-               values(%s, %s, %s, %s) '''
-        x = (e1.get(), e2.get(), e3.get(), d2)
-        cr.execute(sq, x)
-        cn.commit()
-    Clear()
+               values(%s, %s, %s, %s) '''# inserts all fields with the result in the database
+        x = (e1.get(), e2.get(), e3.get(), d2)# gets the name,weight,height,dateentry entries
+        cr.execute(sq, x)#exucuting the sq query to insert the data of x
+        cn.commit()#to make sence and insert(makes the changes)
+    Clear()#clears all fields 
 
-
+#updates any record in database (getting it through name while clicking to view one and you can update the name)
 def Update(a=""):
-    if (not is_num(e2.get())) or float(e2.get()) <= 0:
-        e2.focus()
-        v2.set('')
+    if (not is_num(e2.get())) or float(e2.get()) <= 0:#checks if weight entry isn't number or float
+        e2.focus()#puts the mouse in the weight entry
+        v2.set('')#sets the weight entry string nothing
         return
 
-    if (not is_num(e3.get())) or float(e2.get()) <= 0:
-        e3.focus()
-        v3.set('')
+    if (not is_num(e3.get())) or float(e2.get()) <= 0:#checks if height entry isn't number or float
+        e3.focus()#puts the mouse in the height entry
+        v3.set('')#sets the height entry string nothing
         return
 
-    d2 = datetime.strptime(e4.get(), '%d/%m/%Y')
-    if todaydat <= d2:
-        showerror('Update', 'Date should be < System date')
+    d2 = datetime.strptime(e4.get(), '%d/%m/%Y')#strptime() method creates a datetime object from the given string
+    if todaydat <= d2:# checks if today date is less than or equal to date time of now
+        showerror('Update', 'Date should be < System date')# shows messagebox 
         return False
-    Calcul()
-    quest = messagebox.askyesno("Update", "Sure to update data ?")
-    if quest == True:
-        sq = 'update BMI.Report set Weight=%s, Height=%s, B_Date=%s where Name=%s'
-        x = (e2.get(), e3.get(), d2, e1.get())
-        cr.execute(sq, x)
-        cn.commit()
+    Calcul()#calculates the current entered data of weight and height
+    quest = messagebox.askyesno("Update", "Sure to update data ?")# asks yes or no if you really want to update
+    if quest == True:# if quest = yes then...
+        sq = 'update BMI.Report set Weight=%s, Height=%s, B_Date=%s where Name=%s'# updates the current data entered where name = name entered
+        x = (e2.get(), e3.get(), d2, e1.get())# gets weight,height,dateentry and name entry
+        cr.execute(sq, x)# execute the sq query and the x variable
+        cn.commit()# make changes in the database
 
-    Clear()
+    Clear()#clearing all data in the fields
 
-
+#gets the height and weight entry and calculate it and checks if height or weight is empty and if contains negative num
 def Calcul(a=""):
-    Clear_Lb()
+    Clear_Lb()#runs the clear_lb function whick cofigure the foreground of the category to be unshown
 
-    if (is_num(e2.get())) and float(e2.get()) > 0:
-        w = float(e2.get())
-    else:
-        e2.focus()
-        v2.set('')
+    if (is_num(e2.get())) and float(e2.get()) > 0:#checks if weight entry is number and float
+        w = float(e2.get())#gets the float of the weight
+    else:# if weight entry isn't number (float)
+        e2.focus()#puts the mouse in the weight entry
+        v2.set('')#sets nothing in the weight entry string
         return
 
-    if (is_num(e3.get())) and float(e3.get()) > 0:
-        h = float(e3.get())
-    else:
-        e3.focus()
-        v3.set('')
+    if (is_num(e3.get())) and float(e3.get()) > 0:#checks if height entry is number and float
+        h = float(e3.get())#gets the float of the height
+    else:# if height entry isn't number (float)
+        e3.focus()#puts the mouse in the height entry
+        v3.set('')#sets nothing in the height entry string
         return
 
-    r = float(w / (h ** 2))
+    r = float(w / (h ** 2))#bmi calculation (weight /)
 
     lb11.config(text=str(round(r, 2)))
 
@@ -149,7 +149,7 @@ def Calcul(a=""):
         lb15.config(fg='red')
         lb18.config(fg='red')
 
-
+#view all records from database in a listbox ordereb by name and you can view or delete any record
 def VAll(a=""):
     global ls1
     win2 = Tk()
@@ -191,7 +191,7 @@ def VAll(a=""):
 
     win2.mainloop()
 
-
+#get's the name you entered and printing the data on the fields place by place if record exists and checks if exist
 def VOne(a=""):
     Clear_Lb()
     if e1.get() == "":
@@ -207,7 +207,7 @@ def VOne(a=""):
                 e4.set_date(row[4])
         Calcul()
 
-
+#get's the name you entered and delete it if exists
 def Delete(a=""):
     if e1.get() == "":
         showinfo("Delete", "Please enter the Name")
@@ -227,7 +227,7 @@ def Delete(a=""):
             v1.set('')
             e1.focus()
 
-
+#prints first record place by place
 def Frst():
     global Indextabl
     try:
@@ -247,7 +247,7 @@ def Frst():
     except:
         showinfo("First", " No records was found ")
 
-
+#prints last record place by place
 def Lst():
     global Indextabl
     try:
@@ -267,7 +267,7 @@ def Lst():
     except:
         showwarning("Last", " No records was found ")
 
-
+#prints any record record place by place
 def Prntdata(r):
     v1.set(r[1])
     v2.set(r[2])
@@ -276,7 +276,7 @@ def Prntdata(r):
     Calcul()
     print(Indextabl)
 
-
+#gives you all records ordered by the id(+1)
 def Nxt():
     sqs = "SELECT * FROM BMI.Report order by id_inc"
     cr.execute(sqs)
@@ -292,7 +292,7 @@ def Nxt():
     ctr = str(Indextabl) + '/' + str(len(tbl) - 1)
     lb20.config(text=ctr)
 
-
+#gives you all records ordered by id(-1)
 def Prvs():
     sqs = "SELECT * FROM BMI.Report order by id_inc"
     cr.execute(sqs)
@@ -308,6 +308,7 @@ def Prvs():
         btP['state'] = 'disabled'
     if btN['state'] == 'disabled':
         btN['state'] = 'normal'
+#help menu
 def help_me():
     window = Tk()
     window.title("Help?")
@@ -320,17 +321,17 @@ def help_me():
     label2.pack()
     label2.bind('<Button-1>', lambda e: click_me("https://www.healthline.com/nutrition/18-foods-to-gain-weight#TOC_TITLE_HDR_2"))
     window.mainloop()
-
+#refer to help_me menu
 def click_me(url):
     webbrowser.open_new(url)
 
-
+#deletes any record selected in the view all window list
 def delete_selected():
     id = ls1.get(ANCHOR)[:7].strip()
     cr.execute('delete from BMI.report where id_inc=%s'%id)
     ls1.delete(ANCHOR)
 
-
+#view any record selected in the view all window list
 def view_selected():
     id = ls1.get(ANCHOR)[:7].strip()
     cr.execute('select * from report Where id_inc=%s '%id)
@@ -340,7 +341,7 @@ def view_selected():
             v1.set(row[1])
             VOne()
 
-
+#white mode in the menu colors
 def test():
     global labels
     global buttons
@@ -377,7 +378,7 @@ def brown_yello():
         i.config(fg='yellow', bg = 'brown')
     for o in labels2:
         o.config(bg='brown',fg='brown')
-
+#menu themes
 def default():
     s.theme_use('default')
 def clam():
@@ -388,13 +389,13 @@ def classic():
     s.theme_use('classic')
 def about():
     messagebox.showinfo("About Bmi","Body mass index (BMI) is a measure of body fat based on height and weight that applies to adult men and women")
-
+#it searches if there is a record(refer to the name entered)
 def search_table(x):
     cr.execute('SELECT * FROM BMI.report')
     table = cr.fetchall()
     results = tuple(row for row in table if row[1].startswith(x))
     return results
-
+##refer to search_table + design and showing records
 def search():
     search_win = Tk()
     search_win.title("Result for your search")
@@ -431,28 +432,29 @@ def search():
 win = Tk()
 win.title('Body Mass Index')
 win.geometry('620x420')
-win.bind('<Control-x>', quit)
-win.bind('<Control-s>', Save)
-win.bind('<Control-d>', Delete)
-win.bind('<Control-B>', VAll)
-win.bind('<Control-b>', VOne)
-win.bind('<Control-u>', Update)
-win.bind('<Control-r>', Calcul)
-win.bind('<Control-q>', Clear)
+win.bind('<Control-x>', quit)# for quitting in the menu bar
+win.bind('<Control-s>', Save)# for saving any record to db
+win.bind('<Control-d>', Delete)# for deleting any record in db
+win.bind('<Control-B>', VAll)# for viewing all records in db
+win.bind('<Control-b>', VOne)# fro viewing one record (checks name entry)
+win.bind('<Control-u>', Update)# for updating any record(checks name entry)
+win.bind('<Control-r>', Calcul)# for calculating height and weight
+win.bind('<Control-q>', Clear)# reseting all fields 
 s = ttk.Style()
 menubar = tk.Menu(win)
-image  = PhotoImage(file='exit.png')
-image1 = PhotoImage(file='about.png')
-image2 = PhotoImage(file='theme.png')
-image3 = PhotoImage(file='save.png')
-image4 = PhotoImage(file='delete.png')
-image5 = PhotoImage(file='improve.png')
-image6 = PhotoImage(file='viewall.png')
-image7 = PhotoImage(file='viewone.png')
-image8 = PhotoImage(file='update.png')
-image9 = PhotoImage(file='calculate.png')
-imag10 = PhotoImage(file='color.png')
-image10= PhotoImage(file='clear.png')
+image  = PhotoImage(file='exit.png')# exit png
+image1 = PhotoImage(file='about.png')# about png
+image2 = PhotoImage(file='theme.png')# themes png
+image3 = PhotoImage(file='save.png')# save png
+image4 = PhotoImage(file='delete.png')# delete png
+image5 = PhotoImage(file='improve.png')# improve png
+image6 = PhotoImage(file='viewall.png')#view all png
+image7 = PhotoImage(file='viewone.png')# view one png
+image8 = PhotoImage(file='update.png')#update png
+image9 = PhotoImage(file='calculate.png')#calculate png
+imag10 = PhotoImage(file='color.png')# colors png
+image10= PhotoImage(file='clear.png')#clear png
+#menu bar commands and cascades
 filemenu = tk.Menu(menubar, tearoff=0)
 menubar.add_cascade(label='File', menu=filemenu)
 edit = tk.Menu(menubar, tearoff=0)
@@ -491,39 +493,41 @@ filemenu.add_cascade(label='  Exit                    Ctrl+X', image = image, co
 win.config(menu=menubar)
 
 
+#labels
+lb1 = Label(win, text='BMI CALCULATOR', font=('Arial', 20))#bmi calculator label
+lb2 = Label(win, text='Full Name')# full name label
+lb3 = Label(win, text='Weight in Kg')#weight label
+lb4 = Label(win, text='Height in m')# height label
+lb5 = Label(win, text='Date of Birth')#date of birth label
+#string vars
+v1 = StringVar()# for name entry
+v2 = StringVar()# weight entry
+v3 = StringVar()# height entry
 
-lb1 = Label(win, text='BMI CALCULATOR', font=('Arial', 20))
-lb2 = Label(win, text='Full Name')
-lb3 = Label(win, text='Weight in Kg')
-lb4 = Label(win, text='Height in m')
-lb5 = Label(win, text='Date of Birth')
-v1 = StringVar()
-v2 = StringVar()
-v3 = StringVar()
-
-e1 = Entry(win, textvariable=v1)
-e2 = Entry(win, textvariable=v2, justify='right')
-e3 = Entry(win, textvariable=v3, justify='right')
+e1 = Entry(win, textvariable=v1)# name entry
+e2 = Entry(win, textvariable=v2, justify='right')#weight entry
+e3 = Entry(win, textvariable=v3, justify='right')#height entry
 
 vd = date.today()
 todaydat = datetime(vd.year, vd.month, vd.day)
-e4 = DateEntry(win, width=12, borderwidth=2, day=vd.day, month=vd.month, year=vd.year, date_pattern='dd/mm/yyyy', maxdate = vd)
+e4 = DateEntry(win, width=12, borderwidth=2, day=vd.day, month=vd.month, year=vd.year, date_pattern='dd/mm/yyyy', maxdate = vd)#date entry
 
-bt1 = Button(win, text='New', fg='green', width=8, command=Clear)
-bt2 = Button(win, text='Save', fg='red', width=8, command=Save)
-bt3 = Button(win, text='UpDate', fg='red', width=8, command=Update)
-bt4 = Button(win, text='Calculate', fg='green', width=8, command=Calcul)
-bt5 = Button(win, text='View One', fg='green', width=8, command=VOne)
-bt6 = Button(win, text='View All', fg='green', width=8, command=VAll)
-bt7 = Button(win, text='Delete', fg='red', width=8, command=Delete)
-bt8 = Button(win, text='Last', fg='blue', width=8, command=Lst)
-bt9 = Button(win, text='First', fg='blue', width=8, command=Frst)
-btP = Button(win, text='<<', fg='blue', width=8, command=Prvs)
-btN = Button(win, text='>>', fg='blue', width=8, command=Nxt)
-sech= Button(win, text = 'Search', fg = 'green', width = 8,command = search)
-lb10 = Label(win, text='Your BMI is:')
+bt1 = Button(win, text='New', fg='green', width=8, command=Clear)# clear button
+bt2 = Button(win, text='Save', fg='red', width=8, command=Save)# save button
+bt3 = Button(win, text='UpDate', fg='red', width=8, command=Update)# update button
+bt4 = Button(win, text='Calculate', fg='green', width=8, command=Calcul)# calculate button
+bt5 = Button(win, text='View One', fg='green', width=8, command=VOne)#view one button
+bt6 = Button(win, text='View All', fg='green', width=8, command=VAll)#view all button
+bt7 = Button(win, text='Delete', fg='red', width=8, command=Delete)#delete buttons
+bt8 = Button(win, text='Last', fg='blue', width=8, command=Lst)# last  button
+bt9 = Button(win, text='First', fg='blue', width=8, command=Frst)# first button
+btP = Button(win, text='<<', fg='blue', width=8, command=Prvs)# previous button
+btN = Button(win, text='>>', fg='blue', width=8, command=Nxt)# next button
+sech= Button(win, text = 'Search', fg = 'green', width = 8,command = search)# search button
+lb10 = Label(win, text='Your BMI is:')#your bmi is label
 lb11 = Label(win, text='      ', bg='white', relief=SUNKEN, font=('Arial', 10))
-fr1 =  LabelFrame(win, text='Your Category is', padx=20, pady=20)
+fr1 =  LabelFrame(win, text='Your Category is', padx=20, pady=20)# category label frame
+# category results
 lb12 = Label(fr1, fg='lightgray', text='You Are UnderWeight')
 lb13 = Label(fr1, fg='lightgray', text='You Are Normal')
 lb14 = Label(fr1, fg='lightgray', text='You Are Over Weight')
@@ -533,10 +537,10 @@ lb17 = Label(win, fg='lightgray', text='Yes')
 lb18 = Label(win, fg='lightgray', text='No')
 ctr =  str(Indextabl) + '/' + str(len(tbl) - 1)
 lb20 = Label(win, text=ctr, bg='white', width=4, justify='center')
-labels = [lb1,lb2,lb3,lb4,lb5,lb10,lb11,lb16,lb20,fr1]
-buttons = [bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,btN,btP]
-labels2 = [lb12,lb13,lb14,lb15,lb17,lb18]
-entries = [e1,e2,e3]
+labels = [lb1,lb2,lb3,lb4,lb5,lb10,lb11,lb16,lb20,fr1]#for looping colors for labels
+buttons = [bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,btN,btP]#for looping colors for buttont
+labels2 = [lb12,lb13,lb14,lb15,lb17,lb18]#for looping colors for labels(results)
+#placing every thing
 lb1.place(x=200, y=5)
 lb2.place(x=20, y=80)
 lb3.place(x=20, y=120)
@@ -572,6 +576,5 @@ lb12.pack(anchor=CENTER)
 lb13.pack(anchor=CENTER)
 lb14.pack(anchor=CENTER)
 lb15.pack(anchor=CENTER)
-
-win.protocol('WM_DELETE_WINDOW', Exit)
+win.protocol('WM_DELETE_WINDOW', Exit)# for while closing the window function exit will work
 win.mainloop()
